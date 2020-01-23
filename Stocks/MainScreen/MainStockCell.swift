49b9345ce5a769493e01fecea1c12a9f32258bcb
RoieSC <cohen.roie@gmail.com>
2020-01-23 09:19:57 +0200

    no message

--- Stocks/MainScreen/MainStockCell.swift
+++ Stocks/MainScreen/MainStockCell.swift
@@ -31,7 +31,6 @@ class MainStockCell: UITableViewCell {
         if let imgURL = URL(string: urlStr ?? "") {
             let thumbnailSize = CGSize(width: 100, height: 100)
             stockImgView?.sd_setImage(with: imgURL, placeholderImage: nil, context: [SDWebImageContextOption.imageThumbnailPixelSize: thumbnailSize], progress: nil, completed: { (image, error, cacheType, url) in
-//                print("setImage completed - url: \(String(describing: url?.path)) cacheType: \(cacheType.rawValue)")
                 if error == nil && image != nil {
                     completion(true, nil)
                 }
